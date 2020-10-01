256b732b6442be3a4187224c778e41b77649d523
kevser-tr <kevser.tanrikulu@student.ehb.be>
2020-10-01 20:31:32 +0200

    Task space invader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,16 @@
 "use strict";
 
-alert("Draw your space invader here!");
+let canvas = document.querySelector("canvas");
+let context = canvas.getContext("2d");
+drawSpaceInvader();
+
+function drawSpaceInvader() {
+    context.beginPath();
+    context.rect(25, 200, 250, 100);
+    context.rect(75, 50, 50, 150);
+    context.rect(175, 50, 50, 150);
+    context.rect(25, 100, 50, 50);
+    context.rect(225, 100, 50, 50);
+    context.fillStyle = "#66ff00";
+    context.fill();
+}
